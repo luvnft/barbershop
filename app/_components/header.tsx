b231bddfdca77fd7a03f4d04c684237c0507bac9
guhborda <guh_borda@hotmail.com>
2024-04-14 23:11:18 -0300

    feat: login and menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,19 @@
+"use client"
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react"
+import { signIn } from "next-auth/react";
 const Header = () => {
+    const handleLoginClick = async () => {
+        await signIn();
+    }
     return (
         <Card >
             <CardContent className="py-8 px-5 justify-between flex flex-row">
                 <Image alt="logo" src="/logo.svg" height={22} width={120} />
-                <Button variant="outline" size="icon">
-                    <MenuIcon size={20}/>
+                <Button onClick={handleLoginClick} variant="outline" size="icon">
+                    <MenuIcon size={20} />
                 </Button>
             </CardContent>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`dark`}>
-      <body className={inter.className}>{children}
-        <Footer />
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );

ff79dae39219b84615a35f6442f4556cfa61382c
guhborda <guh_borda@hotmail.com>
2024-04-19 23:11:57 -0300

    Feat: Google Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,20 @@ export default function RootLayout({
         <meta property='og:url' content='https://barbershop-flas.vercel.app' />
         <meta property='og:type' content='website' />
         <link rel='icon' href='../public/logo.svg' />
+        <script
+          dangerouslySetInnerHTML={
+            {
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K4QXKWBK');`,
+            }}
+        />
       </Head>
       <body className={inter.className}>
+
         <AuthProvider>
           {children}
           <Footer />

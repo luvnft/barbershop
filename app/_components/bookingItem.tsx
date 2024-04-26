aa34661dcd023fcd7ba1f5720e3afd7b73463dd4
guhborda <guh_borda@hotmail.com>
2024-04-25 22:27:12 -0300

    Feat: delete bookings

--- app/_components/bookingItem.tsx
+++ app/_components/bookingItem.tsx
@@ -1,11 +1,18 @@
+"use client"
 import { Card, CardContent } from "./ui/card"
 import { Badge } from "./ui/badge"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Booking, Prisma } from "@prisma/client"
 import { format, isFuture } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
 import Image from "next/image";
+import { Button } from "./ui/button"
+import { cancelBooking } from "../_actions/cancel-booking"
+import { toast } from "sonner"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
+
 interface BookingItemProps {
     booking: Prisma.BookingGetPayload<{
         include: {
@@ -15,7 +22,20 @@ interface BookingItemProps {
     }>;
 }
 const BookingItem = ({ booking }: BookingItemProps) => {
+    const [isDeleteLoading, setIsDeleteLoading] = useState(false);
     const isBookingConfirmed = isFuture(booking.date);
+
+    const handleCancelClick = async () => {
+        setIsDeleteLoading(true)
+        try {
+            await cancelBooking(booking.id);
+            toast.success("Reserva cancelada com sucesso")
+        } catch (error) {
+
+        } finally {
+            setIsDeleteLoading(false);
+        }
+    }
     return (
         <Sheet>
             <SheetTrigger asChild>
@@ -50,7 +70,9 @@ const BookingItem = ({ booking }: BookingItemProps) => {
                 </Card>
             </SheetTrigger>
             <SheetContent>
-                <SheetHeader ><SheetTitle className="text-left">Informações da Reserva</SheetTitle></SheetHeader>
+                <SheetHeader >
+                    <SheetTitle className="text-left">Informações da Reserva</SheetTitle>
+                </SheetHeader>
                 <div className={`relative w-full h-fit mt-5`}>
                     <Card className="flex items-center justify-start">
                         <CardContent className="flex items-center gap-2 px-5 py-5 justify-start">
@@ -67,7 +89,10 @@ const BookingItem = ({ booking }: BookingItemProps) => {
                     </Card>
 
                 </div>
+
+                <div className="pt-3">{isBookingConfirmed ? (<Badge className="bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit">Agendado</Badge>) : (<Badge className="bg-secondary text-white hover:bg-secondary w-fit">Finalizado</Badge>)}</div>
                 <div className="py-3 ">
+
                     <Card>
                         <CardContent className=" flex flex-col p-2 gap-2">
                             <div className="flex justify-between">
@@ -88,13 +113,18 @@ const BookingItem = ({ booking }: BookingItemProps) => {
                                 <h1 className="text-gray-400 text-sm">Horário</h1>
                                 <h4 className="text-sm">{format(booking.date, "hh:mm", { locale: ptBR })}</h4>
                             </div>
-
-
-
-
                         </CardContent>
                     </Card>
                 </div>
+
+                <div className="absolute w-full justify-center gap-2 bottom-0 left-0 flex h-fit-content px-5">
+                    <SheetClose asChild><Button className="relative bg-secondary bottom-3 w-full hover:bg-secondary ">
+                        Voltar</Button></SheetClose>
+                    <Button className="relative bg-red-600 bottom-3 w-full hover:bg-red-500" onClick={handleCancelClick} disabled={!isBookingConfirmed || isDeleteLoading}>
+                        {isDeleteLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                        Cancelar Reserva</Button>
+                </div>
+
             </SheetContent>
         </Sheet >
     )

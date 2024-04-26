2078e7d2dd96feab256a5f228f0d827730ee599f
guhborda <guh_borda@hotmail.com>
2024-04-25 22:36:28 -0300

    Feat: delete bookings

--- app/barbershops/[id]/_actions/save-bookings.ts
+++ app/barbershops/[id]/_actions/save-bookings.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { db } from "@/app/_lib/prisma";
+import { revalidatePath } from "next/cache";
 
 interface SaveBookingParams {
     barbershopId: string;
@@ -17,4 +18,6 @@ export const saveBooking = async (params: SaveBookingParams) => {
             barbershopId: params.barbershopId,
         }
     })
+    revalidatePath("/")
+    revalidatePath("/bookings")
 }

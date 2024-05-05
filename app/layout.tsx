3ba3de27c9b0614e5c7e59685d36d28842b2fc22
guhborda1 <guhborda@gmail.com>
2024-05-05 12:03:20 -0300

    Feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,11 @@ export default function RootLayout({
         />
         <meta property='og:url' content='https://barbershop-flas.vercel.app' />
         <meta property='og:type' content='website' />
-        <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="3any" href="favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../public/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../public/images/favicon-16x16.png" />
         <link rel="manifest" href="../public/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#603cba" />
         <meta name="theme-color" content="#603cba" />

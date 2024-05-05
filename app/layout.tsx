0d101e2474cdeef5866cd98ff5a47105a6ab4dd4
guhborda1 <guhborda@gmail.com>
2024-05-05 11:57:05 -0300

    Feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         />
         <meta property='og:url' content='https://barbershop-flas.vercel.app' />
         <meta property='og:type' content='website' />
-        <link rel='icon' type="image/svg" href='../public/logo.svg' />
         <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
